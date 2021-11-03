fd8970040227d2e9c31867ada318d6035fcf0548
Katiana Kontolati <kkontol1@jhu.edu>
2021-11-03 17:10:03 -0400

    Updated PCE unit tests

--- tests/Surrogates/test_PCE.py
+++ tests/Surrogates/test_PCE.py
@@ -1,63 +1,83 @@
-from UQpy.Distributions import Uniform
+from UQpy.Distributions import *
 from UQpy.Surrogates import *
 import numpy as np
 
 np.random.seed(1)
-dist = Uniform(loc=0, scale=10)
 max_degree, n_samples = 2, 10
-polys = Polynomials(dist_object=dist, degree=max_degree)
-
+dist = Uniform(loc=0, scale=10)
+pce = PolyChaosExp(dist)
 
 def func(x):
     return x * np.sin(x) / 10
 
 x = dist.rvs(n_samples)
+x_test = dist.rvs(n_samples)
 y = func(x)
 
-def poly_func(x):
-    p = polys.evaluate(x)
+def poly_td_func(pce, max_degree):
+    construct_td_basis(pce, max_degree)
+    p = pce.poly_basis
     return p
 
-def pce_model(polys, x, y):
-    lstsq = PolyChaosLstsq(poly_object=polys)
-    pce = PCE(method=lstsq)
-    pce.fit(x, y)
-    return pce
+def poly_tp_func(pce, max_degree):
+    construct_tp_basis(pce, max_degree)
+    p = pce.poly_basis
+    return p
 
-def pce_predict(x, c):
-    x_ = poly_func(x)
-    return x_.dot(c)
+def pce_coeff(pce, x, y):
+    fit_lstsq(pce, x, y)
+    return pce.coefficients
 
+def pce_predict(pce,x):
+    return pce.predict(x)
 
 # Unit tests
-
 def test_1():
     """
-    Test Polynomials class
+    Test td basis
     """
-    assert round(poly_func(x)[1, 1], 3) == 0.763
-
+    assert round(poly_td_func(pce, max_degree)[1].eval_basis(x)[0], 4) == 0.3301
 
 def test_2():
     """
-    Test PCE class coefficients
+    Test tp basis
     """
-    assert round(float(pce_model(polys, x, y).C[1]), 3) == 0.328
-
+    assert round(poly_tp_func(pce, max_degree)[1].eval_basis(x)[0], 4) == 0.3301
 
 def test_3():
     """
-    Test PCE class prediction
+    Test PCE coefficients
     """
-    model = pce_model(polys, x, y)
-    assert round(float(pce_predict(x, model.C)[1]), 3) == 0.338
-
+    assert round(pce_coeff(pce, x, y)[0][0], 4) == 0.0051
 
 def test_4():
     """
-    Test Error Estimation class
+    Test PCE prediction
     """
-    x_val = x + np.random.rand(1)
-    y_val = func(x_val)
-    error = ErrorEstimation(surr_object=pce_model(polys, x, y))
-    assert round(error.validation(x_val, y_val), 3) == 0.432
+    y_test = pce_predict(pce, x_test)
+    assert round(y_test[0][0], 4) == -0.1607
+
+def test_5():
+    """
+    Test Sobol indices
+    """
+    assert round(pce_sobol_first(pce)[0][0], 3) == 1.0
+
+def test_6():
+    """
+    Test Sobol indices
+    """
+    assert round(pce_sobol_total(pce)[0][0], 3) == 1.0
+
+def test_7():
+    """
+    PCE mean
+    """
+    assert round(pce_mean(pce)[0][0], 3) == 0.218
+
+def test_8():
+    """
+    PCE variance
+    """
+    assert round(pce_variance(pce)[0][0], 3) == 0.185
+
